c0b24771cd4845f885814d73354e9482b09b8f1b
Arkadiusz Zygan <a.zygan@gmail.com>
2020-05-05 23:31:27 +0200

    Add logic to join component

--- client/src/components/Join/Join.js
+++ client/src/components/Join/Join.js
@@ -1,11 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Link } from "react-router-dom";
+
+import './Join.scss';
 
 const Join = () => {
+  const [name, setName] = useState('');
+  const [room, setRoom] = useState('');
 
   return(
-    <h1>
-      Join
-    </h1>
+    <div>
+      <div>
+        <h1>Dołącz</h1>
+        <div>
+          <input type='text' placeholder='name' onChange={(event) => setName(event.target.value)} />
+        </div>
+        <div>
+          <input type='text' placeholder='room' onChange={(event) => setRoom(event.target.value)} />
+        </div>
+        <Link onClick={event => (!name || !room) ? event.preventDefault() : null} to={`/chat?name=${name}&room=${room}`}>
+          <button type='submit'>Wejdź</button>
+        </Link>
+      </div>
+    </div>
   )
 };
 
