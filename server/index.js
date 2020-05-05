82e751517a480603111ff31ce59a7a78a200bf03
Arkadiusz Zygan <a.zygan@gmail.com>
2020-05-05 23:03:53 +0200

    Add socket.io to back

--- server/index.js
+++ server/index.js
@@ -4,9 +4,22 @@ const socketio = require('socket.io');
 
 const PORT = process.env.PORT || 5000;
 
+const router = require('./router');
+
 const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
+
+app.use(router);
+
+io.on('connection', (socket) => {
+  console.log('Amazing... New connection');
+
+  socket.on('disconnect', () => {
+    console.log('User had left :( ')
+  });
+
+});
 
 
 server.listen(PORT, () => console.log(`Wonderful!! Server has started on ${PORT}`));
