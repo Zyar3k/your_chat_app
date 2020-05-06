e6a14e9cf32b2e5c76854b8288ab3dc55b9e522d
Arkadiusz Zygan <a.zygan@gmail.com>
2020-05-06 16:31:48 +0200

    Add style to join components

--- client/src/components/Join/Join.js
+++ client/src/components/Join/Join.js
@@ -8,21 +8,26 @@ const Join = () => {
   const [room, setRoom] = useState('');
 
   return(
-    <div>
-      <div>
-        <h1>Dołącz</h1>
-        <div>
-          <input type='text' placeholder='name' onChange={(event) => setName(event.target.value)} />
+    <div className='join'>
+      <div className='joinContainer'>
+        <h1>Cz@-App</h1>
+        <p>
+          Wybierz swój nick, wybierz pokój, zaporś przyjaciół, rozmawiaj do woli...
+        </p>
+        <div className='inputWrapper'>
+          <label >Imię</label>
+          <input name='name' type='text' onChange={(event) => setName(event.target.value)} />
         </div>
-        <div>
-          <input type='text' placeholder='room' onChange={(event) => setRoom(event.target.value)} />
+        <div className='inputWrapper'>
+          <label >Twój pokój</label>
+          <input name='room' type='text' onChange={(event) => setRoom(event.target.value)} />
         </div>
         <Link onClick={event => (!name || !room) ? event.preventDefault() : null} to={`/chat?name=${name}&room=${room}`}>
-          <button type='submit'>Wejdź</button>
+          <button className='joinButton' type='submit'>Wejdź</button>
         </Link>
       </div>
     </div>
-  )
+  );
 };
 
 export default Join;
--- client/src/components/features/Input/Input.js
+++ client/src/components/features/Input/Input.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './Input.scss';
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
 
 const Input = ({ message, sendMessage, setMessage }) => (
 <form>
