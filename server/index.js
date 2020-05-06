7d0d3c6791b5463eeb87f1638b944adb6065a6e2
Arkadiusz Zygan <a.zygan@gmail.com>
2020-05-06 12:40:11 +0200

    Prepareing back for messages

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const http = require('http');
 const express = require('express');
 const socketio = require('socket.io');
+const { addUser, removeUser, getUser, getUsersInRoom } = require('./users');
 
 const PORT = process.env.PORT || 5000;
 
@@ -13,10 +14,15 @@ const io = socketio(server);
 app.use(router);
 
 io.on('connection', (socket) => {
-  console.log('Amazing... New connection');
+  // console.log('Amazing... New connection');
 
   socket.on('join', ({ name, room }, callback) => {
-    console.log(name, room)
+    // console.log(name, room)
+    const { error, user } = addUser({ id: socket.id, name, room});
+
+    if(error) return callback(error);
+
+    socket.join(user.room);
     
   });
 
